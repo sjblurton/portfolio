fea35580b32d3c7d6adcb1f7c396f5d6205291e0
Simon Blurton <sblurton@hotmail.co.uk>
2021-11-15 08:26:09 -0600

    removed netlify pluging

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
-    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
