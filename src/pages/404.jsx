9e71b2d8ae726b6ca7bcdd6cedb8b11074202528
Simon Blurton <sblurton@hotmail.co.uk>
2022-02-09 21:04:58 -0600

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,11 +1,14 @@
 import React from "react"
+import { Container } from "../styles/404.js"
 import { Seo, Layout } from "../components/layout"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Container>
   </Layout>
 )
 
---
+++ src/styles/404.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  color: ${props => props.theme.primary};
+`
