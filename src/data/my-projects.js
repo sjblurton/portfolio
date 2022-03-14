10baa7ad51cc78b4965c5f62087b178b038de377
Simon Blurton <sblurton@hotmail.co.uk>
2022-03-14 12:59:06 -0600

    update: new project added, old projects removed.

--- src/data/my-projects.js
+++ src/data/my-projects.js
@@ -28,12 +28,12 @@ export const projectData = [
     live: "https://big-6-next-js.vercel.app",
   },
   {
-    title: "Vertgo Rock Climbing",
+    title: "VCLIMB",
     id: "fb2ee4bb-f8a1-51c2-b19e-5a5977badd48",
     image: (
       <StaticImage
-        src="../assets/work/vertigo.png"
-        alt="Vertgo Rock Climbingportfolio mock up"
+        src="../assets/work/vclimb.png"
+        alt="VCLIMB portfolio mock up"
         placeholder="blurred"
         imgStyle={{
           width: "100%",
@@ -41,39 +41,9 @@ export const projectData = [
         }}
       />
     ),
-    description: "A website  with two languages, built in Gatsby using...",
-    technologies: [
-      "Styled Components",
-      "Formik, and Yup",
-      "SEO",
-      "SVG animation",
-    ],
-    code: "https://github.com/sjblurton/vertigo-in-gatsby",
-    live: "https://vertigo-gatsby.netlify.app/en/",
-  },
-  {
-    title: "ECOEVuk website",
-    id: "6b69c819-780e-517f-b198-0410877039a5",
-    image: (
-      <StaticImage
-        src="../assets/work/ecoev.png"
-        alt="ECOEVukportfolio mock up"
-        placeholder="blurred"
-        imgStyle={{
-          width: "100%",
-          borderRadius: "10px 10px 0px 0px",
-        }}
-      />
-    ),
-    description:
-      "Electric car charging installation website built in Gatsby using...",
-    technologies: [
-      "Styled Components",
-      "Formik, and Yup",
-      "SEO",
-      "SVG animation",
-    ],
-    code: "https://github.com/sjblurton/ecoevuk",
-    live: "https://ecoevuk.netlify.app/",
+    description: "A full stack CRUD app, built in NEXT.JS using...",
+    technologies: ["Tailwind", "Formik, and Yup", "SEO", "Postgres REST API"],
+    code: "https://github.com/sjblurton/v-climb-nextjs",
+    live: "https://vclimb.vercel.app/",
   },
 ]
